afcd442ef3b1c2ef52df11d06bb4e8aaf4cc148c
morten <morten.sogard.jensen@gmail.com>
2024-10-17 07:17:17 +0000

    day 6

--- advent-of-code-2020/day6/task.go
+++ advent-of-code-2020/day6/task.go
@@ -1,20 +1,37 @@
 package day6
 
 import (
-	"fmt"
 	"strings"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	sum := 0
+	for _, group := range strings.Split(input, "\n\n") {
+		seen := map[rune]bool{}
+		for _, line := range strings.Split(group, "\n") {
+			for _, r := range line {
+				seen[r] = true
+			}
+		}
+		sum += len(seen)
 	}
-	return 0
+	return sum
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	sum := 0
+	for _, group := range strings.Split(input, "\n\n") {
+		seen := map[rune]int{}
+		members := strings.Split(group, "\n")
+		mlen := len(members)
+		for _, line := range members {
+			for _, r := range line {
+				seen[r] += 1
+				if seen[r] == mlen {
+					sum += 1
+				}
+			}
+		}
 	}
-	return 0
+	return sum
 }
--- advent-of-code-2020/day6/task_test.go
+++ advent-of-code-2020/day6/task_test.go
@@ -8,11 +8,39 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 11, part1(`abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`))
+	require.Equal(t, 6549, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 6, part2(`abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`))
+	require.Equal(t, 3466, part2(utils.GetInput(t, "input.txt")))
 }
