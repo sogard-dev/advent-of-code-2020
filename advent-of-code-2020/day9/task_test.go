68ba412ab5f70631f04f8ea042a1ed1d17ba26a2
morten <morten.sogard.jensen@gmail.com>
2024-10-19 15:27:25 +0000

    day 9

--- advent-of-code-2020/day9/task_test.go
+++ advent-of-code-2020/day9/task_test.go
@@ -7,12 +7,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const testInput string = `35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`
+
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 127, part1(testInput, 5))
+	require.Equal(t, 57195069, part1(utils.GetInput(t, "input.txt"), 25))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 62, part2(testInput, 5))
+	require.Equal(t, 7409241, part2(utils.GetInput(t, "input.txt"), 25))
 }
