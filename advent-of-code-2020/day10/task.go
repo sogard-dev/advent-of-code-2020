a2f0fdf2015a356e7363670d6bad45e569cf6e91
morten <morten.sogard.jensen@gmail.com>
2024-10-20 10:54:21 +0000

    day 10

--- advent-of-code-2020/day10/task.go
+++ advent-of-code-2020/day10/task.go
@@ -1,20 +1,58 @@
 package day10
 
 import (
-	"fmt"
-	"strings"
+	"slices"
+
+	"github.com/sogard-dev/advent-of-code-2020/utils"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	nums := utils.GetAllNumbers(input)
+	slices.Sort(nums)
+
+	jumps := make([]int, 4)
+	prev := 0
+	for _, n := range nums {
+		jumps[n-prev] += 1
+		prev = n
 	}
-	return 0
+	jumps[3] += 1
+	return jumps[1] * jumps[3]
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	nums := utils.GetAllNumbers(input)
+	slices.Sort(nums)
+
+	return count(nums, 0, 0, map[entry]int{})
+}
+
+type entry struct {
+	idx        int
+	lastPicked int
+}
+
+func count(nums []int, idx int, lastPicked int, cache map[entry]int) int {
+	device := nums[len(nums)-1]
+	if lastPicked == device {
+		return 1
 	}
-	return 0
+	if idx >= len(nums) {
+		return 0
+	}
+	n := nums[idx]
+	if n-lastPicked > 3 {
+		return 0
+	}
+
+	e := entry{idx: idx, lastPicked: lastPicked}
+	if p, exist := cache[e]; exist {
+		return p
+	}
+
+	options := 0
+	options += count(nums, idx+1, n, cache)
+	options += count(nums, idx+1, lastPicked, cache)
+	cache[entry{idx: idx, lastPicked: lastPicked}] = options
+	return options
 }
--- advent-of-code-2020/day10/task_test.go
+++ advent-of-code-2020/day10/task_test.go
@@ -7,12 +7,44 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const testInput string = `28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3`
+
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 22*10, part1(testInput))
+	require.Equal(t, 2400, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 19208, part2(testInput))
+	require.Equal(t, 338510590509056, part2(utils.GetInput(t, "input.txt")))
 }
