09f20c5934df9774a79640fc49b1304fb63b8ab0
morten <morten.sogard.jensen@gmail.com>
2024-10-21 11:34:03 +0000

    day 13

--- advent-of-code-2020/day13/task.go
+++ advent-of-code-2020/day13/task.go
@@ -1,20 +1,105 @@
 package day13
 
 import (
-	"fmt"
+	"math"
+	"slices"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2020/utils"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	nums := utils.GetAllNumbers(input)
+	earliest := nums[0]
+	busses := nums[1:]
+
+	waitingTime := math.MaxInt
+	closestBus := 0
+	for _, bus := range busses {
+		time := bus - (earliest % bus)
+
+		if time < waitingTime {
+			waitingTime = time
+			closestBus = bus
+		}
 	}
-	return 0
+
+	return waitingTime * closestBus
+}
+
+type number struct {
+	num    int
+	offset int
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	line := strings.Split(input, "\n")[1]
+	entries := strings.Split(line, ",")
+	numbers := []number{}
+	for i, e := range entries {
+		n := utils.GetAllNumbers(e)
+		if len(n) == 1 {
+			numbers = append(numbers, number{
+				offset: i,
+				num:    n[0],
+			})
+		}
 	}
-	return 0
+
+	slices.SortFunc(numbers, func(a, b number) int {
+		return b.num - a.num
+	})
+
+	base, mult := findCommonT(numbers[0:min(4, len(numbers))])
+
+	for i := 0; i < math.MaxInt; i++ {
+		t := base + mult*i
+
+		good := true
+		for _, e := range numbers {
+			if (t+e.offset)%e.num != 0 {
+				good = false
+				break
+			}
+		}
+
+		if good {
+			return t
+		}
+	}
+
+	return -1
+}
+
+func findCommonT(numbers []number) (int, int) {
+	biggest := number{}
+	for _, e := range numbers {
+		if e.num > biggest.num {
+			biggest = e
+		}
+	}
+
+	base := 0
+
+	for i := 1; i < math.MaxInt; i++ {
+		t := biggest.num*i - biggest.offset
+
+		good := true
+
+		for _, e := range numbers {
+			if (t+e.offset)%e.num != 0 {
+				good = false
+				break
+			}
+		}
+
+		if good {
+			if base == 0 {
+				base = t
+			} else {
+				return base, t - base
+			}
+		}
+	}
+	panic("donkey")
 }
--- advent-of-code-2020/day13/task_test.go
+++ advent-of-code-2020/day13/task_test.go
@@ -7,12 +7,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const testInput = `939
+7,13,x,x,59,x,31,19`
+
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 295, part1(testInput))
+	require.Equal(t, 2406, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 3417, part2(`q
+	17,x,13,19`))
+	require.Equal(t, 1068781, part2(testInput))
+	require.Equal(t, 225850756401039, part2(utils.GetInput(t, "input.txt")))
 }
