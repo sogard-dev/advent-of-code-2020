22a83c2d1338b5963c05b17893b0a4050afd6ed6
morten <morten.sogard.jensen@gmail.com>
2024-10-22 05:32:27 +0000

    refactor day 13

--- advent-of-code-2020/day13/task.go
+++ advent-of-code-2020/day13/task.go
@@ -2,7 +2,6 @@ package day13
 
 import (
 	"math"
-	"slices"
 	"strings"
 
 	"github.com/sogard-dev/advent-of-code-2020/utils"
@@ -46,60 +45,17 @@ func part2(input string) int {
 		}
 	}
 
-	slices.SortFunc(numbers, func(a, b number) int {
-		return b.num - a.num
-	})
-
-	base, mult := findCommonT(numbers[0:min(4, len(numbers))])
-
-	for i := 0; i < math.MaxInt; i++ {
-		t := base + mult*i
-
-		good := true
-		for _, e := range numbers {
-			if (t+e.offset)%e.num != 0 {
-				good = false
-				break
-			}
-		}
-
-		if good {
-			return t
-		}
-	}
-
-	return -1
-}
-
-func findCommonT(numbers []number) (int, int) {
-	biggest := number{}
+	d := 0
+	m := 1
 	for _, e := range numbers {
-		if e.num > biggest.num {
-			biggest = e
-		}
-	}
-
-	base := 0
-
-	for i := 1; i < math.MaxInt; i++ {
-		t := biggest.num*i - biggest.offset
-
-		good := true
-
-		for _, e := range numbers {
-			if (t+e.offset)%e.num != 0 {
-				good = false
+		for {
+			d += m
+			if (d+e.offset)%e.num == 0 {
+				m = m * e.num
 				break
 			}
 		}
-
-		if good {
-			if base == 0 {
-				base = t
-			} else {
-				return base, t - base
-			}
-		}
 	}
-	panic("donkey")
+
+	return d
 }
