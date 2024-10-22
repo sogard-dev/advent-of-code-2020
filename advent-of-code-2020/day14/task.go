bacef9de4741a648282652d4e68d263869dd631b
morten <morten.sogard.jensen@gmail.com>
2024-10-22 08:47:16 +0000

    day 14

--- advent-of-code-2020/day14/task.go
+++ advent-of-code-2020/day14/task.go
@@ -1,20 +1,112 @@
 package day14
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2020/utils"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+type machine struct {
+	memory  map[uint64]uint64
+	mask    string
+	bitsize int
+	v1      bool
 }
 
-func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+func numToPaddedRune(value uint64, bitsize int) []rune {
+	asStr := strconv.FormatUint(value, 2)
+	asStr = strings.Repeat("0", bitsize-len(asStr)) + asStr
+	return []rune(asStr)
+}
+
+func replaceRunes(runes []rune, chars string, mask string) {
+	for i, r := range mask {
+		if strings.ContainsRune(chars, r) {
+			runes[i] = r
+		}
 	}
-	return 0
+}
+
+func replaceValueByMask(mask string, value uint64, bitsize int) uint64 {
+	out := numToPaddedRune(value, bitsize)
+	replaceRunes(out, "01", mask)
+	n, ok := strconv.ParseUint(string(out), 2, 64)
+	if ok != nil {
+		panic("ohh noes")
+	}
+	return n
+}
+
+func setFloatingMemory(m *machine, out []rune, idx int, value uint64) {
+	if idx == m.bitsize {
+		n, ok := strconv.ParseUint(string(out), 2, 64)
+		if ok != nil {
+			panic("ohh noes")
+		}
+		m.memory[n] = value
+	} else if out[idx] == 'X' {
+		out[idx] = '0'
+		setFloatingMemory(m, out, idx+1, value)
+		out[idx] = '1'
+		setFloatingMemory(m, out, idx+1, value)
+		out[idx] = 'X'
+	} else {
+		setFloatingMemory(m, out, idx+1, value)
+	}
+}
+
+func (m *machine) set(address, value uint64) {
+	if m.v1 {
+		m.memory[address] = replaceValueByMask(m.mask, value, m.bitsize)
+	} else {
+		out := numToPaddedRune(address, m.bitsize)
+		replaceRunes(out, "X1", m.mask)
+		setFloatingMemory(m, out, 0, value)
+	}
+}
+
+func (m *machine) setMask(mask string) {
+	m.mask = mask
+}
+
+func (m *machine) sum() uint64 {
+	s := uint64(0)
+	for _, v := range m.memory {
+		s += v
+	}
+	return s
+}
+
+func newMachine(bitsize int, part1 bool) machine {
+	return machine{
+		mask:    strings.Repeat("X", bitsize),
+		bitsize: bitsize,
+		memory:  map[uint64]uint64{},
+		v1:      part1,
+	}
+}
+
+func part1(input string) uint64 {
+	m := newMachine(36, true)
+	return solve(input, m)
+}
+
+func part2(input string) uint64 {
+	m := newMachine(36, false)
+	return solve(input, m)
+}
+
+func solve(input string, m machine) uint64 {
+	for _, line := range strings.Split(input, "\n") {
+		spl := strings.Split(line, " = ")
+		if spl[0] == "mask" {
+			m.setMask(spl[1])
+		} else {
+			nums := utils.GetAllNumbers(line)
+			address, value := nums[0], nums[1]
+			m.set(uint64(address), uint64(value))
+		}
+	}
+	return m.sum()
 }
--- advent-of-code-2020/day14/task_test.go
+++ advent-of-code-2020/day14/task_test.go
@@ -8,11 +8,17 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 165, int(part1(`mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0`)))
+	require.Equal(t, 15172047086292, int(part1(utils.GetInput(t, "input.txt"))))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 208, int(part2(`mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`)))
+	require.Equal(t, 4197941339968, int(part2(utils.GetInput(t, "input.txt"))))
 }
