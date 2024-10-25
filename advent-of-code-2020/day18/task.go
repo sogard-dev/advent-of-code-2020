5815d68b08cf040c75bf4efdfc6a8bd37f3d04b1
morten <morten.sogard.jensen@gmail.com>
2024-10-25 06:07:39 +0000

    day 18

--- advent-of-code-2020/day18/task.go
+++ advent-of-code-2020/day18/task.go
@@ -1,20 +1,122 @@
 package day18
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2020/utils"
 )
 
+func strToTokens(line string) []string {
+	line = strings.ReplaceAll(line, "(", "( ")
+	line = strings.ReplaceAll(line, ")", " )")
+	tokens := strings.Split(line, " ")
+	return tokens
+}
+
 func part1(input string) int {
+	sum := 0
 	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+		lineSum, _ := evaluateExpressionPart1(strToTokens(line), 0)
+		sum += lineSum
 	}
-	return 0
+	return sum
 }
 
 func part2(input string) int {
+	sum := 0
 	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+		lineSum, _ := evaluateExpressionPart2(strToTokens(line), 0)
+		sum += lineSum
 	}
-	return 0
+	return sum
+}
+
+func evaluateExpressionPart1(tokens []string, from int) (int, int) {
+	sum := 0
+
+	add := func(i int) {
+		sum += i
+	}
+	mult := func(i int) {
+		sum *= i
+	}
+
+	op := add
+
+	for i := from; i < len(tokens); i++ {
+		t := tokens[i]
+		switch t {
+		case "(":
+			subSum, subIndex := evaluateExpressionPart1(tokens, i+1)
+			op(subSum)
+			i = subIndex
+		case ")":
+			return sum, i
+		case "+":
+			op = add
+		case "*":
+			op = mult
+		default:
+			op(utils.GetAllNumbers(t)[0])
+		}
+	}
+
+	return sum, -1
+}
+
+func evaluateExpressionPart2(tokens []string, from int) (int, int) {
+	evalutatedTokens := []string{}
+
+	i := from
+	cont := true
+	for ; i < len(tokens) && cont; i++ {
+		t := tokens[i]
+		switch t {
+		case "(":
+			subSum, subIndex := evaluateExpressionPart2(tokens, i+1)
+			evalutatedTokens = append(evalutatedTokens, strconv.Itoa(subSum))
+			i = subIndex - 1
+		case ")":
+			cont = false
+		default:
+			evalutatedTokens = append(evalutatedTokens, t)
+		}
+	}
+
+	sum := 0
+	for len(evalutatedTokens) > 0 {
+		if len(evalutatedTokens) == 1 {
+			sum = utils.GetAllNumbers(evalutatedTokens[0])[0]
+			break
+		} else {
+			reduce := func(k int, op func(int, int) int) {
+				s := op(utils.GetAllNumbers(evalutatedTokens[k-1])[0], utils.GetAllNumbers(evalutatedTokens[k+1])[0])
+				newEvalutatedTokens := []string{}
+				newEvalutatedTokens = append(newEvalutatedTokens, evalutatedTokens[:k-1]...)
+				newEvalutatedTokens = append(newEvalutatedTokens, strconv.Itoa(s))
+				newEvalutatedTokens = append(newEvalutatedTokens, evalutatedTokens[k+2:]...)
+				evalutatedTokens = newEvalutatedTokens
+			}
+
+			for k := 1; k < len(evalutatedTokens); k += 2 {
+				if evalutatedTokens[k] == "+" {
+					reduce(k, func(a, b int) int {
+						return a + b
+					})
+					k -= 2
+				}
+			}
+			for k := 1; k < len(evalutatedTokens); k += 2 {
+				if evalutatedTokens[k] == "*" {
+					reduce(k, func(a, b int) int {
+						return a * b
+					})
+					k -= 2
+				}
+			}
+		}
+	}
+
+	return sum, i
 }
--- advent-of-code-2020/day18/task_test.go
+++ advent-of-code-2020/day18/task_test.go
@@ -8,11 +8,16 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 26, part1(`2 * 3 + (4 * 5)`))
+	require.Equal(t, 437, part1(`5 + (8 * 3 + 9 + 3 * 4 * 3)`))
+	require.Equal(t, 12240, part1(`5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))`))
+	require.Equal(t, 13632, part1(`((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2`))
+	require.Equal(t, 21993583522852, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 1445, part2(`5 + (8 * 3 + 9 + 3 * 4 * 3)`))
+	require.Equal(t, 51, part2(`1 + (2 * 3) + (4 * (5 + 6))`))
+	require.Equal(t, 669060, part2(`5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))`))
+	require.Equal(t, 122438593522757, part2(utils.GetInput(t, "input.txt")))
 }
