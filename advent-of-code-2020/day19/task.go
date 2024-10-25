5f27e4e180bccb3e15cdb0e89c172c42f33021aa
morten <morten.sogard.jensen@gmail.com>
2024-10-25 09:51:49 +0000

    day 19

--- advent-of-code-2020/day19/task.go
+++ advent-of-code-2020/day19/task.go
@@ -2,19 +2,105 @@ package day19
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+type rule interface {
+	evaluate(rules *map[string]rule, depth int) string
+}
+
+type or struct {
+	left, right []string
+}
+
+type single struct {
+	s []string
+}
+
+type literal struct {
+	v string
+}
+
+func (s single) evaluate(rules *map[string]rule, depth int) string {
+	str := ""
+	for _, l := range s.s {
+		str += (*rules)[l].evaluate(rules, depth+1)
 	}
-	return 0
+	return str
+}
+
+func (o or) evaluate(rules *map[string]rule, depth int) string {
+	if depth > 100 {
+		return ""
+	}
+
+	leftSide := ""
+	for _, l := range o.left {
+		leftSide += (*rules)[l].evaluate(rules, depth+1)
+	}
+
+	rightSide := ""
+	for _, r := range o.right {
+		rightSide += (*rules)[r].evaluate(rules, depth+1)
+	}
+
+	return "(" + leftSide + "|" + rightSide + ")"
+}
+
+func (l literal) evaluate(_ *map[string]rule, _ int) string {
+	return l.v
+}
+
+func part1(input string) int {
+	return solve(input)
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	input = strings.ReplaceAll(input, "8: 42", "8: 42 | 42 8")
+	input = strings.ReplaceAll(input, "11: 42 31", "11: 42 31 | 42 11 31")
+	return solve(input)
+}
+
+func solve(input string) int {
+	blocks := strings.Split(input, "\n\n")
+	rulesBlock := blocks[0]
+
+	rules := map[string]rule{}
+	for _, line := range strings.Split(rulesBlock, "\n") {
+		spl := strings.Split(line, ": ")
+		ruleNo := spl[0]
+		ruleStr := spl[1]
+		if strings.Contains(ruleStr, "|") {
+			sides := strings.Split(ruleStr, " | ")
+			rules[ruleNo] = or{
+				left:  strings.Split(sides[0], " "),
+				right: strings.Split(sides[1], " "),
+			}
+		} else if strings.Contains(ruleStr, `"`) {
+			rules[ruleNo] = literal{
+				v: ruleStr[len(ruleStr)-2 : len(ruleStr)-1],
+			}
+		} else {
+			rules[ruleNo] = single{
+				s: strings.Split(ruleStr, " "),
+			}
+		}
+
 	}
-	return 0
+
+	eval0 := rules["0"].evaluate(&rules, 0)
+	fmt.Println("Rule 0: ", eval0)
+	r, _ := regexp.Compile("^" + eval0 + "$")
+
+	sum := 0
+	messagesBlock := blocks[1]
+	for _, line := range strings.Split(messagesBlock, "\n") {
+		if r.MatchString(line) {
+			fmt.Println(line)
+			sum += 1
+		}
+	}
+
+	return sum
 }
--- advent-of-code-2020/day19/task_test.go
+++ advent-of-code-2020/day19/task_test.go
@@ -8,11 +8,68 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 2, part1(`0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: "a"
+5: "b"
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb`))
+	require.Equal(t, 134, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
+	require.Equal(t, 12, part2(`42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba`))
 	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
 }
