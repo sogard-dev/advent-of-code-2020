f62860938c39a709861c0f8775687d9395a58964
morten <morten.sogard.jensen@gmail.com>
2024-10-28 08:29:20 +0000

    day 21

--- advent-of-code-2020/day21/task.go
+++ advent-of-code-2020/day21/task.go
@@ -1,20 +1,98 @@
 package day21
 
 import (
-	"fmt"
+	"maps"
+	"slices"
 	"strings"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	allIngredients, allAllergens := solve(input)
+
+	for _, i := range allAllergens {
+		delete(allIngredients, i)
 	}
-	return 0
+
+	sum := 0
+	for _, v := range allIngredients {
+		sum += v
+	}
+
+	return sum
 }
 
-func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+func part2(input string) string {
+	_, allAllergens := solve(input)
+
+	allergensSorted := slices.Collect(maps.Keys(allAllergens))
+	slices.Sort(allergensSorted)
+
+	canon := []string{}
+	for _, a := range allergensSorted {
+		canon = append(canon, allAllergens[a])
 	}
-	return 0
+
+	return strings.Join(canon, ",")
+}
+
+func solve(input string) (map[string]int, map[string]string) {
+	input = strings.ReplaceAll(input, ")", "")
+
+	allIngredients := map[string]int{}
+	allAllergens := map[string]map[string]bool{}
+
+	for _, line := range strings.Split(input, "\n") {
+		spl := strings.Split(line, " (contains ")
+
+		ingredients := strings.Split(spl[0], " ")
+		for _, v := range ingredients {
+			allIngredients[v] += 1
+		}
+
+		allergens := strings.Split(spl[1], ", ")
+		for _, a := range allergens {
+			if prevIngredients, exist := allAllergens[a]; !exist {
+				allAllergens[a] = map[string]bool{}
+
+				for _, v := range ingredients {
+					allAllergens[a][v] = true
+				}
+			} else {
+				for prev := range prevIngredients {
+					if !slices.Contains(ingredients, prev) {
+						delete(allAllergens[a], prev)
+					}
+				}
+			}
+		}
+	}
+
+	changed := true
+	for changed {
+		changed = false
+		for _, ingredients := range allAllergens {
+			if len(ingredients) == 1 {
+				for i := range ingredients {
+					for _, otherIngredients := range allAllergens {
+						if _, exist := otherIngredients[i]; exist && len(otherIngredients) > 1 {
+							delete(otherIngredients, i)
+							changed = true
+						}
+					}
+				}
+			}
+		}
+	}
+
+	allergenMapping := map[string]string{}
+
+	for a, ingredients := range allAllergens {
+		if len(ingredients) != 1 {
+			panic("ohh noes")
+		}
+		for i := range ingredients {
+			allergenMapping[a] = i
+		}
+	}
+	return allIngredients, allergenMapping
 }
--- advent-of-code-2020/day21/task_test.go
+++ advent-of-code-2020/day21/task_test.go
@@ -7,12 +7,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const testInput = `mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)`
+
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 5, part1(testInput))
+	require.Equal(t, 2061, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, "mxmxvkd,sqjhc,fvjkl", part2(testInput))
+	require.Equal(t, "cdqvp,dglm,zhqjs,rbpg,xvtrfz,tgmzqjz,mfqgx,rffqhl", part2(utils.GetInput(t, "input.txt")))
 }
