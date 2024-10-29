d7c103095be9a7f88fde4cebae17a0f1b63888a2
morten <morten.sogard.jensen@gmail.com>
2024-10-29 11:22:18 +0000

    day 23

--- advent-of-code-2020/day23/task.go
+++ advent-of-code-2020/day23/task.go
@@ -1,20 +1,157 @@
 package day23
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2020/utils"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+func parse(input string) (circle, int) {
+	c := newCircle()
+	first := -1
+	for _, line := range strings.Split(input, "") {
+		n := utils.GetAllNumbers(line)[0]
+		if first == -1 {
+			first = n
+		}
+		c.insert(n)
 	}
-	return 0
+	return c, first
+}
+
+func part1(input string, moves int) string {
+	cubs, first := parse(input)
+	solve(&cubs, first, moves)
+
+	ret := cubs.removeAfter(1, cubs.size-1)
+
+	str := ""
+	for _, v := range ret {
+		str = str + strconv.Itoa(v)
+
+	}
+
+	return str
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	cubs, first := parse(input)
+	for c := cubs.size + 1; c <= 1000000; c++ {
+		cubs.insert(c)
 	}
-	return 0
+
+	solve(&cubs, first, 10000000)
+	ret := cubs.removeAfter(1, 2)
+	return ret[0] * ret[1]
+}
+
+func solve(cubs *circle, first int, moves int) {
+	currentCub := first
+	TO_TAKE := 3
+	MAX := cubs.size
+	for move := range moves {
+		if move%10000 == 0 {
+			println("Move ", move)
+		}
+
+		pickUp := cubs.removeAfter(currentCub, TO_TAKE)
+
+		destination := currentCub - 1
+		for !cubs.insertAfter(destination, pickUp) {
+			destination--
+			if destination < 1 {
+				destination = MAX
+			}
+		}
+
+		currentCub = cubs.valueAfter(currentCub)
+	}
+}
+
+func newCircle() circle {
+	return circle{
+		lookup: map[int]*node{},
+	}
+}
+
+type circle struct {
+	head   *node
+	size   int
+	lookup map[int]*node
+}
+
+func (c *circle) insert(v int) {
+	n := node{
+		v: v,
+	}
+	if c.head == nil {
+		c.head = &n
+		n.next = &n
+	} else {
+		n.next = c.head.next
+		c.head.next = &n
+		c.head = &n
+	}
+	c.size++
+	c.lookup[v] = &n
+}
+
+func (c *circle) removeAfter(v int, occurences int) []int {
+	ret := make([]int, occurences)
+	n := c.lookup[v]
+
+	nextHead := n
+	for r := 0; r < occurences; r++ {
+		nextHead = nextHead.next
+		ret[r] = nextHead.v
+		delete(c.lookup, nextHead.v)
+	}
+	n.next = nextHead.next
+	c.head = n
+	c.size -= occurences
+	return ret
+}
+
+func (c *circle) insertAfter(v int, values []int) bool {
+	if n, exist := c.lookup[v]; exist {
+		insertAfter := n
+		for _, toInsert := range values {
+			newNode := node{
+				v:    toInsert,
+				next: insertAfter.next,
+			}
+			insertAfter.next = &newNode
+			insertAfter = &newNode
+			c.lookup[toInsert] = &newNode
+		}
+		c.size += len(values)
+
+		return true
+	}
+
+	return false
+}
+
+func (c *circle) valueAfter(v int) int {
+	n := c.lookup[v]
+	return n.next.v
+}
+
+func (c *circle) print() {
+	var visited *node
+	for n := c.head; n.next != nil; n = n.next {
+		if visited == nil {
+			visited = n
+		} else if visited == n {
+			break
+		}
+
+		print(" ", n.v, " ")
+	}
+}
+
+type node struct {
+	v    int
+	next *node
 }
--- advent-of-code-2020/day23/task_test.go
+++ advent-of-code-2020/day23/task_test.go
@@ -3,16 +3,16 @@ package day23
 import (
 	"testing"
 
-	"github.com/sogard-dev/advent-of-code-2020/utils"
 	"github.com/stretchr/testify/require"
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, "92658374", part1(`389125467`, 10))
+	require.Equal(t, "67384529", part1(`389125467`, 100))
+	require.Equal(t, "26354798", part1("284573961", 100))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 149245887792, part2(`389125467`))
+	require.Equal(t, 166298218695, part2(`284573961`))
 }
